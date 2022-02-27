a66657f3818b4b3996e182fd27a656fc9c620fe1
saddamhosan <contact.saddamhosan@gmail.com>
2022-02-27 19:22:15 +0600

    update

---
+++ app.js
@@ -0,0 +1,3 @@
+const apiKey = "api_key=2d55cf2b891b4fa7fe1176c75ce5eeeb";
+const baseUrl = "https://api.themoviedb.org/3";
+const apiUrl = baseUrl + "/discover/movie?sort_by=popularity.desc&" + apiKey;
